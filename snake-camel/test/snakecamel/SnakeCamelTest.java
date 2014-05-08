f815f35b8634aac84c4c793b20e98bf4ea216899
ujiharakaede <ujiharakaede@192.168.13.100>
2014-05-08 12:26:54 +0900

    変更途中です

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -9,9 +9,36 @@ public class SnakeCamelTest {
 	@Test
 	public void test() {
 		SnakeCamelUtil scu = new SnakeCamelUtil(); 
-		String ac1 = scu.snakeToCamelcase("abcdefg");
-		String ac2 = scu.camelToSnakecase("abcdefg");
-		System.out.println(ac1);
-		System.out.println(ac2);
+	//	String c1 = scu.capitalize("");
+		//System.out.println(c1);
+		String c2 = scu.capitalize("a");
+		System.out.println(c2);
+		String c3 = scu.capitalize("xyz");
+		System.out.println(c3);
+		
+//		String u1 = scu.uncapitalize("");
+	//	System.out.println(u1);
+		String u2 = scu.uncapitalize("A");
+		System.out.println(u2);
+		String u3 = scu.uncapitalize("Xyz");
+		System.out.println(u3);
+		
+		String cc1 = scu.snakeToCamelcase("abc");
+		System.out.println(cc1);
+		String cc2 = scu.snakeToCamelcase("abc_def");
+		System.out.println(cc2);
+		String cc3 = scu.snakeToCamelcase("abc_def_gh");
+		System.out.println(cc3);
+		String cc4 = scu.snakeToCamelcase("abc__def___gh");
+		System.out.println(cc4);
+		String cc5 = scu.snakeToCamelcase("_abc_def__");
+		System.out.println(cc5);
+		
+		String sc1 = scu.camelToSnakecase("Abc");
+		System.out.println(sc1);
+		String sc2 = scu.camelToSnakecase("AbcDef");
+		System.out.println(sc2);
+		String sc3 = scu.camelToSnakecase("AbcDefGh");
+		System.out.println(sc3);
 	}
 }
